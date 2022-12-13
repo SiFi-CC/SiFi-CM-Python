6a0f037ba6bce23e7b3a8a115a6b89f3dd4cedc1
VitaliiUr <vitaliyurbanevich@gmail.com>
2022-12-13 12:59:01 +0100

    mura

--- sifi_cm/mura.py
+++ sifi_cm/mura.py
@@ -31,16 +31,40 @@ def get_mura(order):
     return mask
 
 
-def get_mura_cut(order, cutx, cuty=None):
+def get_mura_cut(order, cutx=None, cuty=None, one_dim=False):
     """Return 2D MURA array
     It is not a classical way.
     Comparing to the wikipedia algorytm the mask is inverted
     """
-    if not cuty:
+    if cutx and not cuty:
         cuty = cutx
-    mask = get_mura(order)
-    mask_cut = mask[order//2-cutx//2:order//2+cutx // 2 + 1,
-                    order//2-cuty//2:order//2+cuty//2 + 1]
-    mask_cut[0, :] = 1
-    mask_cut[:, -1] = 0
-    return mask_cut
+    if one_dim:
+        mask = get_mura_1d(order)
+    else:
+        mask = get_mura(order)
+    print(mask.shape)
+    if cutx and cuty:
+        mask = mask[order//2-cuty//2:order//2+cuty // 2 + 1,
+                        order//2-cutx//2:order//2+cutx//2 + 1]
+    if not one_dim:
+        mask[0, :] = 1
+        mask[:, -1] = 0
+    return mask
+
+
+def get_mura_1d(order):
+    """Return 1D MURA array
+    It is not a classical way.
+    Comparing to the wikipedia algorytm the mask is inverted
+    """
+    if not is_prime(order):
+        raise ValueError("The mask order should be prime")
+    quadratic_residues = np.unique(np.arange(order)**2 % order)
+    mask = np.zeros(order)
+    c = -np.ones(order)
+    c[np.isin(np.arange(order), quadratic_residues)] = 1
+    # cc = np.outer(c, c)
+    mask[np.where(c == 1)] = 1
+    mask = np.abs(mask - 1)[::-1]
+    mask = np.repeat(mask[:, np.newaxis], order, axis=1).T
+    return mask
